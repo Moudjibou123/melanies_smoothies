21e437cba3f83fbcff1b186f42a4918e7163549c
Moudjibou123 <moudjibou.ousseini@gmail.com>
2024-06-20 10:02:24 +0200

    Update streamlit_app.py
    Change Connexion syntaxe

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 
-cnx=st.connexion("Snowflake")
+cnx=st.connection("Snowflake")
 session =cnx.session()
 
 # Write directly to the app
